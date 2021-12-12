ce66475a73ce4d0d57d394072e4e0ee4ca668cd1
Juan Andres Mendez <ja.mendez@uniandes.edu.co>
2021-12-11 22:14:51 -0500

    tree imp adv

---
+++ Search_Algo/Day1.py
@@ -0,0 +1,113 @@
+from re import S
+import unittest
+
+
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        if self.isEmpty():
+            return None
+        return self.items[len(self.items) - 1]
+
+    def size(self):
+        return len(self.items)
+
+
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def enqueue(self, item):
+        self.items.insert(0, item)
+
+    def dequeue(self):
+        if self.isEmpty():
+            return None
+        return self.items.pop()
+
+    def front(self):
+        return self.items[- 1]
+
+    def back(self):
+        return self.items[0]
+
+    def size(self):
+        return len(self.items)
+# Unit Test
+
+
+class TestStack(unittest.TestCase):
+    def setUp(self):
+        self.stack = Stack()
+        self.stack.push(1)
+        self.stack.push(2)
+        self.stack.push(3)
+
+    def test_pop(self):
+        self.stack.pop()
+        self.assertEqual(self.stack.size(), 2)
+        self.stack.pop()
+        self.assertEqual(self.stack.size(), 1)
+        self.stack.pop()
+        self.assertEqual(self.stack.size(), 0)
+
+    def test_size(self):
+        self.assertEqual(self.stack.size(), 3)
+        self.stack.push(4)
+        self.assertEqual(self.stack.size(), 4)
+        self.stack.pop()
+        self.assertEqual(self.stack.size(), 3)
+
+    def test_peek(self):
+        self.assertEqual(self.stack.peek(), 3)
+        self.stack.pop()
+        self.assertEqual(self.stack.peek(), 2)
+        self.stack.pop()
+        self.assertEqual(self.stack.peek(), 1)
+        self.stack.pop()
+        self.assertEqual(self.stack.peek(), None)
+
+    def test_stack(self):
+        stack = Stack()
+        self.assertEqual(stack.isEmpty(), True)
+        stack.push(4)
+        stack.push('dog')
+        self.assertEqual(stack.peek(), 'dog')
+        self.assertEqual(stack.size(), 2)
+        self.assertEqual(stack.isEmpty(), False)
+        self.assertEqual(stack.pop(), 'dog')
+        self.assertEqual(stack.pop(), 4)
+        self.assertEqual(stack.isEmpty(), True)
+
+
+class TestQueue(unittest.TestCase):
+    def setUp(self):
+        self.queue = Queue()
+        self.queue.enqueue('perro')
+        self.queue.enqueue('gato')
+
+    def testPop(self):
+        self.assertEqual(self.queue.dequeue(), 'perro')
+        self.assertEqual(self.queue.size(), 1)
+        self.assertEqual(self.queue.dequeue(), 'gato')
+        self.assertEqual(self.queue.size(), 0)
+        self.assertEqual(self.queue.dequeue(), None)
+
+
+# run the test
+if __name__ == '__main__':
+    unittest.main()
--- Search_Algo/Tree.py
+++ Search_Algo/Tree.py
@@ -1,1 +1,43 @@
 import unittest
+
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, data):
+        if self.value == data:
+            return False
+        elif self.value > data:
+            if self.left:
+                return self.left.insert(data)
+            else:
+                self.left = Node(data)
+                return True
+        else:
+            if self.right:
+                return self.right.insert(data)
+            else:
+                self.right = Node(data)
+                return True
+
+    def isValue(self, data):
+        if self.value == data:
+            return True
+        elif self.value > data:
+            if self.left:
+                return self.left.isValue(data)
+            else:
+                return False
+        else:
+            if self.right:
+                return self.right.isValue(data)
+            else:
+                return False
+
+    def delete(self, data):
+        if self.value == data:
+            if self.left is None and self.right is None:
+                return None
